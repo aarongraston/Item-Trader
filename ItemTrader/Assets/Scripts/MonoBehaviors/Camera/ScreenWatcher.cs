b6be40fcf83202d86118cc9629360999b4932a42
Aaron G <aarongraston@gmail.com>
2019-01-11 19:14:42 -0500

    Working on camera movement

---
+++ ItemTrader/Assets/Scripts/MonoBehaviors/Camera/ScreenWatcher.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWatcher : MonoBehaviour
+{
+    //private variables
+    private Camera theCam;
+    private Vector3 Detector = Vector3.zero;
+    private Transform playerTransform;
+    private bool camMoving;
+    private float screenWidth;
+    private float screenHeight;
+
+    //public variables
+    public GameObject player;
+    public float camMoveSpeed = 1f;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        theCam = gameObject.GetComponent<Camera>();
+        camMoving = false;
+        screenWidth = theCam.pixelWidth;
+        screenHeight = theCam.pixelHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerTransform = player.transform;
+        Detector = theCam.WorldToViewportPoint(playerTransform.position);
+
+        while (!camMoving)
+        {
+            if (Detector.x <= 0)
+            {
+                camMoving = true;
+                StartCoroutine("moveLeft");
+                return;
+            }
+        }
+
+
+
+    }
+
+    public IEnumerator moveLeft() {
+
+        float amountLeft = screenWidth;
+
+        while (amountLeft > 0) {
+            amountLeft -= camMoveSpeed;
+            this.transform.position += new Vector3(-camMoveSpeed, 0, 0);
+        }
+
+        amountLeft = screenWidth;
+        camMoving = false;
+
+        yield return null;
+    } 
+}
---
+++ ItemTrader/Assets/Scripts/MonoBehaviors/Player/MovementRight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementRight : MonoBehaviour
+{
+
+    public float moveAmount = 1;
+    Vector3 move = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Vector3.zero;
+        move = new Vector3(-moveAmount, 0, 0);
+        this.transform.position += move;   
+    }
+}
