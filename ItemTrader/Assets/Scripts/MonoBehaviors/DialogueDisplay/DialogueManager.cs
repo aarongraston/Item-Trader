1e11d47aefd1161ac7145ac8dfe786f255d15a73
Aaron G <aarongraston@gmail.com>
2019-01-25 18:25:03 -0500

    Dialogue elements set properly on canvas

--- ItemTrader/Assets/Scripts/MonoBehaviors/DialogueDisplay/DialogueManager.cs
+++ ItemTrader/Assets/Scripts/MonoBehaviors/DialogueDisplay/DialogueManager.cs
@@ -7,8 +7,9 @@ public class DialogueManager : MonoBehaviour
     public Camera cam;
     public GameObject canvas;
     public GameObject textBoxContainer;
+    public DialogueDisplayVariables variables;
 
-    private void FixedUpdate()
+    private void Update()
     {
         DisplayOverhead();
     }
@@ -18,10 +19,8 @@ public class DialogueManager : MonoBehaviour
         Vector2 viewPortPoint = cam.WorldToViewportPoint(transform.position);
 
         Vector2 WorldObject_ScreenPosition = new Vector2(
-((viewPortPoint.x * canvas.GetComponent<RectTransform>().sizeDelta.x) - (canvas.GetComponent<RectTransform>().sizeDelta.x * 0.5f)),
-((viewPortPoint.y * canvas.GetComponent<RectTransform>().sizeDelta.y) - (canvas.GetComponent<RectTransform>().sizeDelta.y * 0.5f)));
-
-       ;
+((viewPortPoint.x * canvas.GetComponent<RectTransform>().sizeDelta.x + variables.DialogueDisplayOffsetX) - ((canvas.GetComponent<RectTransform>().sizeDelta.x + variables.DialogueDisplayOffsetX) * 0.5f)),
+((viewPortPoint.y * canvas.GetComponent<RectTransform>().sizeDelta.y + variables.DialogueDisplayOffsetY) - ((canvas.GetComponent<RectTransform>().sizeDelta.y + variables.DialogueDisplayOffsetY) * 0.5f)));
 
         textBoxContainer.GetComponent<RectTransform>().anchoredPosition = WorldObject_ScreenPosition;
     }
---
+++ ItemTrader/Assets/Scripts/ScriptableObjects/Dialogue/DialogueDisplayVariables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Dialogue Display/Dialogue Display Variable")]
+public class DialogueDisplayVariables : ScriptableObject
+{
+    public float DialogueDisplayOffsetY = 15f;
+    public float DialogueDisplayOffsetX = -15f;
+}
