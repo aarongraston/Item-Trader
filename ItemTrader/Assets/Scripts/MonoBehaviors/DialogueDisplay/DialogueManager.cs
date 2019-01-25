1fd308180976d04d6b488b69aaa9e458b548ba8b
Aaron G <aarongraston@gmail.com>
2019-01-25 17:41:45 -0500

    Starting with dialogue, potential to crash at this point

---
+++ ItemTrader/Assets/Scripts/MonoBehaviors/DialogueDisplay/DialogueManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject textBoxContainer;
+
+    private void FixedUpdate()
+    {
+        DisplayOverhead();
+    }
+
+    private void DisplayOverhead()
+    {
+        Vector3 viewPortPoint = cam.WorldToViewportPoint(transform.position);
+
+        Debug.Log(viewPortPoint);
+        Debug.Log(textBoxContainer.GetComponent<RectTransform>().anchoredPosition);
+
+        textBoxContainer.GetComponent<RectTransform>().anchoredPosition = viewPortPoint;
+    }
+}
