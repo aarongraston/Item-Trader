691e77c07f0839729626a3514603f68f7667b3ad
Aaron G <aarongraston@gmail.com>
2019-01-26 11:33:19 -0500

    Ugh, I don't know what to do besides write a character state machine. Because also why not

---
+++ ItemTrader/Assets/Scripts/MonoBehaviors/Characters/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+
+    DialogueObject nullDialogue;
+    DialogueObject[] dialogue;
+
+    public bool LookForItem(ItemObject item)
+    {
+        foreach ( DialogueObject d in dialogue)
+        {
+            if (d.item == item)
+            {
+                ExecuteDialogue(d);
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    private void ExecuteDialogue(DialogueObject d)
+    {
+
+    }
+}
