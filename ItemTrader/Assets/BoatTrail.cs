4e0bcf0d963634939e1918df51211f92391a197f
Aaron G <aarongraston@gmail.com>
2019-06-17 13:45:41 -0400

    changes made

---
+++ ItemTrader/Assets/BoatTrail.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatTrail : MonoBehaviour
+{
+    private float yPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yPos = this.transform.position.y; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+    }
+}
---
+++ ItemTrader/Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationSpeed = 0.5f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, rotationSpeed));
+        
+    }
+}
